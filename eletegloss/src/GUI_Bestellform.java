db2247f308be6a666448b7cec1ea68c2c2f03541
EnzoGratz <gratz.enzo@hakspittal.at>
2025-06-11 11:03:23 +0200

    GUI überarbeitet

--- eletegloss/src/GUI_Bestellform.java
+++ eletegloss/src/GUI_Bestellform.java
@@ -6,19 +6,48 @@ package eletegloss.src;
 
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class GUI_Bestellform extends JFrame{
 
-    JLabel txt_Namepaket, txt_PreisPakte;
-    JComboBox cbo_Paket;
+    private JLabel lblUeberschrift, lblName, lblPaket, lblExtras, lblAnzahl, lblTel, lblZahlung;
+    private JTextField txtName, txtTel;
+    private JComboBox<String> cboPaket;
+    private JComboBox<Integer> cboAnzahl;
+    private JCheckBox chkVersiegelung, chkFelgen, chkInnenreinigung, chkAbholer;
+    private JRadioButton optPaypal, optBar;
+    private JButton btnReset, btnDatei, btnDb, btnBeenden;
 
     public GUI_Bestellform() {
+        this.setTitle("Elitegloss – Paket-Bestellung");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    
+        initComponents();
+
+        pack();
+        this.setVisible(true);
+    }
+    
+
+    private void initComponents(){
         setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        Font fontLabel  = new Font("Segoe UI", Font.PLAIN, 13);
+        Font fontTitel  = new Font("Segoe UI", Font.BOLD, 24);
+        Font fontButton = new Font("Segoe UI", Font.BOLD, 15);
+
+        // Überschrift
+        lblUeberschrift = new JLabel("Elitegloss – Dein Fahrzeug, dein Glanz");
+        lblUeberschrift.setFont(fontLabel);
+        lblUeberschrift.setForeground(new Color(0x003366));
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.insets = new Insets(20, 10, 15, 10);
+
+        this.add(lblUeberschrift,c);
 
 
     }
-    
 }
